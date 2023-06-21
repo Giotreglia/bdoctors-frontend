60f80b4820b60ec292396feea322acfc402123ec
Giotreglia <giovanni.treglia@gmail.com>
2023-06-21 18:49:41 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script>
+import AppHome from './components/AppHome.vue'
+
+export default {
+  components: {
+    AppHome
+  }
+}
+</script>
+
+<template>
+  <AppHome />
+</template>
+
+<style lang="scss">
+@import './styles/general.scss';
+</style>
---
+++ src/components/AppHome.vue
@@ -0,0 +1,14 @@
+<script>
+export default {
+  name: 'AppHome'
+}
+
+
+
+</script>
+
+<template>
+  <h1>BDoctors</h1>
+</template>
+
+<style scoped></style>
