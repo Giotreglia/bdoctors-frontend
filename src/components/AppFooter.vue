37796ef1590c86e338d2b8d5f3df5d7041b6b2ef
Alessandro Carrozza <alessandrowcarrozza@gmail.com>
2023-06-22 17:11:25 +0200

    working on footer

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,6 +1,66 @@
+
+<script>
+</script>
+
 <template>
-    <div>
-        ciao footer
-    </div>
+    <footer>
+        <div class="container up d-flex justify-content-between">
+            <div class="side-links" v-for="sides in 3">
+                <ul>
+                    <li>
+                        <h5>title-link</h5>
+                    </li>
+                    <li><a href="#">link</a></li>
+                    <li><a href="#">link</a></li>
+                    <li><a href="#">link</a></li>
+                    <li><a href="#">link</a></li>
+                </ul>
+            </div>
+
+            <div class="side-info">
+                <ul>
+                    <li>
+                        <h1>BDoctors</h1>
+                    </li>
+                    <li>info</li>
+                    <li>info</li>
+                    <li>info</li>
+                    <div>
+                        <span v-for="socials in 4"> social </span>
+                    </div>
+                </ul>
+            </div>
+        </div>
+
+        <div class="container down text-center">
+            <div class="p-3">info</div>
+            <div class="text text-secondary">bdoctors 2023 &copy; Prenota la tua visita online!</div>
+        </div>
+    </footer>
 </template>
-<script></script>
+
+<style lang="scss" scoped>
+.container.up {
+    border-bottom: 1px solid lightgrey;
+
+    li {
+        list-style: none;
+    }
+
+    a {
+        text-decoration: none;
+        color: grey;
+
+        &:hover {
+            text-decoration: underline;
+        }
+    }
+
+}
+
+.container.down {
+    .text {
+        font-size: 14px;
+    }
+}
+</style>
