194726a77c22799010f2ed0d460db5b61e5b5660
Giotreglia <giovanni.treglia@gmail.com>
2023-06-30 15:46:54 +0200

    grafica doc in evidenza

--- src/components/AppPremiumDoc.vue
+++ src/components/AppPremiumDoc.vue
@@ -18,20 +18,28 @@ export default {
 
 <template>
     <div class="row">
-        <h2 class="py-3 text-light">Dottori in evidenza</h2>
         <div v-if="this.store.sponsoredDoctors.length > 0" class="d-flex flex-wrap ms-gap">
 
             <div class="ms-col d-flex align-items-stretch " v-for=" doctor  in  this.store.sponsoredDoctors ">
-                <div class="card my_height ">
+                <div class="card my_height border border-4">
+                    <span class="ms_badge text-bg-success position-absolute top-0 end-0 p-2">In
+                        evidenza <i class="fa-solid fa-star text-warning"></i></span>
                     <img v-if="doctor.photo" :src="`${store.baseUrlnoApi}/storage/${doctor.photo}`"
                         class="card-img-top my_image" :alt="`immagine-profilo-di-${doctor.user.name}`">
-                    <img v-else src="https://www.diamedica.it/wp-content/uploads/2018/12/dottore-1024x1024.jpg"
+                    <img v-else class="card-img-top my_image"
+                        src="https://www.diamedica.it/wp-content/uploads/2018/12/dottore-1024x1024.jpg"
                         :alt="`immagine-profilo-di-${doctor.user.name}`">
                     <div class="card-body position-relative">
                         <h3 class="card-title">{{ doctor.user.name }} {{ doctor.user.surname }}</h3>
-                        <span class="card-title" v-if="doctor.review_count != 0">N. recensioni: {{ doctor.review_count
-                        }}</span>
-                        <span class="card-title d-block" v-else>Nessuna recensione</span>
+                        <div class="badge rounded-pill text-bg-success fs-6 me-2 my-2 d-inline-block"
+                            v-for="specialization in doctor.specializations">{{ specialization.name }}</div>
+                        <div>
+                            <span class="card-title" v-if="doctor.review_count != 0">N. recensioni: {{
+                                doctor.review_count
+                            }}
+                            </span>
+                            <span class="card-title d-block" v-else>Nessuna recensione</span>
+                        </div>
                         <div v-if="doctor.average_stars != 0">
                             <span class="card-title">Media Voti:</span>
                             <div class="btn-outline-dark">
@@ -41,21 +49,12 @@ export default {
                             </div>
                         </div>
                         <span class="card-title d-block" v-else>Nessuna votazione</span>
-                        <p class="card-text">Indirizzo: {{ doctor.address }}</p>
-                        <div class="mb-5">
-
-                            <span class="badge rounded-pill text-bg-success fs-6 me-2 my-2 d-inline-block"
-                                v-for="specialization in doctor.specializations">{{ specialization.name }}</span>
-
-                        </div>
+                        <p class="card-text mb-5">Indirizzo: {{ doctor.address }}</p>
                         <router-link :to="{ name: 'doctor_page', params: { slug: doctor.slug } }"
                             class="btn ms-bg-primary position-absolute my_position">Vedi dettaglio Dottore</router-link>
                     </div>
                 </div>
             </div>
-        </div>
-        <div v-else>
-            <h1>Non ci sono dottori sponsorizzati</h1>
         </div>
     </div>
 </template>
@@ -91,6 +90,11 @@ export default {
 
 .my_height {
     width: 100%;
+}
+
+.ms_badge {
+    border-bottom-left-radius: 12px;
+    border-top-right-radius: 6px;
 }
 
 @media screen and (max-width: 1200px) {
--- src/pages/AppDoctorList.vue
+++ src/pages/AppDoctorList.vue
@@ -102,93 +102,98 @@ export default {
             <router-link @click="this.getListDoc()" :to="{ name: 'doctor_list' }" id="search-btn"
                 class="btn btn-lg z-3 btn-success m-3" type="button">Cerca</router-link>
         </div>
+        <div id="row-container" v-if="this.store.sponsoredDoctors.length > 0 || this.store.doctors.length > 0">
 
-        <div class="row">
-            <h2 class="py-3 text-success">Dottori in evidenza</h2>
-            <div v-if="this.store.sponsoredDoctors.length > 0" class="d-flex flex-wrap ms-gap">
+            <div class="row">
+                <!-- <h2 class="py-3 text-success">Dottori in evidenza</h2> -->
+                <div v-if="this.store.sponsoredDoctors.length > 0" class="d-flex flex-wrap ms-gap">
 
-                <div class="ms-col d-flex align-items-stretch " v-for=" doctor  in  this.store.sponsoredDoctors ">
-                    <div class="card my_height border border-4 border-success">
-                        <img v-if="doctor.photo" :src="`${store.baseUrlnoApi}/storage/${doctor.photo}`"
-                            class="card-img-top my_image" :alt="`immagine-profilo-di-${doctor.user.name}`">
-                        <img v-else class="card-img-top my_image"
-                            src="https://www.diamedica.it/wp-content/uploads/2018/12/dottore-1024x1024.jpg"
-                            :alt="`immagine-profilo-di-${doctor.user.name}`">
-                        <div class="card-body position-relative">
-                            <h3 class="card-title">{{ doctor.user.name }} {{ doctor.user.surname }}</h3>
-                            <span class="card-title" v-if="doctor.review_count != 0">N. recensioni: {{ doctor.review_count
-                            }}
-                            </span>
-                            <span class="card-title d-block" v-else>Nessuna recensione</span>
-                            <div v-if="doctor.average_stars != 0">
-                                <span class="card-title">Media Voti:</span>
-                                <div class="btn-outline-dark">
-                                    <i class="fa-solid fa-star text-warning"
-                                        v-for=" littlestar  in  doctor.average_stars"></i>
-                                    <i class="fa-regular fa-star text-warning"
-                                        v-for=" littlestar  in  (5 - doctor.average_stars)"></i>
+                    <div class="ms-col d-flex align-items-stretch " v-for=" doctor  in  this.store.sponsoredDoctors ">
+                        <div class="card my_height border border-4 border-success">
+                            <span class="ms_badge text-bg-success position-absolute top-0 end-0 p-2">In
+                                evidenza <i class="fa-solid fa-star text-warning"></i></span>
+                            <img v-if="doctor.photo" :src="`${store.baseUrlnoApi}/storage/${doctor.photo}`"
+                                class="card-img-top my_image" :alt="`immagine-profilo-di-${doctor.user.name}`">
+                            <img v-else class="card-img-top my_image"
+                                src="https://www.diamedica.it/wp-content/uploads/2018/12/dottore-1024x1024.jpg"
+                                :alt="`immagine-profilo-di-${doctor.user.name}`">
+                            <div class="card-body position-relative">
+                                <h3 class="card-title">{{ doctor.user.name }} {{ doctor.user.surname }}</h3>
+                                <div class="badge rounded-pill text-bg-success fs-6 me-2 my-2 d-inline-block"
+                                    v-for="specialization in doctor.specializations">{{ specialization.name }}</div>
+                                <div>
+                                    <span class="card-title" v-if="doctor.review_count != 0">N. recensioni: {{
+                                        doctor.review_count
+                                    }}
+                                    </span>
+                                    <span class="card-title d-block" v-else>Nessuna recensione</span>
                                 </div>
+                                <div v-if="doctor.average_stars != 0">
+                                    <span class="card-title">Media Voti:</span>
+                                    <div class="btn-outline-dark">
+                                        <i class="fa-solid fa-star text-warning"
+                                            v-for=" littlestar  in  doctor.average_stars"></i>
+                                        <i class="fa-regular fa-star text-warning"
+                                            v-for=" littlestar  in  (5 - doctor.average_stars)"></i>
+                                    </div>
+                                </div>
+                                <span class="card-title d-block" v-else>Nessuna votazione</span>
+                                <p class="card-text mb-5">Indirizzo: {{ doctor.address }}</p>
+                                <router-link :to="{ name: 'doctor_page', params: { slug: doctor.slug } }"
+                                    class="btn ms-bg-primary position-absolute my_position">Vedi dettaglio
+                                    Dottore</router-link>
                             </div>
-                            <span class="card-title d-block" v-else>Nessuna votazione</span>
-                            <p class="card-text">Indirizzo: {{ doctor.address }}</p>
-                            <div class="mb-5">
-
-                                <span class="badge rounded-pill text-bg-success fs-6 me-2 my-2 d-inline-block"
-                                    v-for="specialization in doctor.specializations">{{ specialization.name }}</span>
-
-                            </div>
-                            <router-link :to="{ name: 'doctor_page', params: { slug: doctor.slug } }"
-                                class="btn ms-bg-primary position-absolute my_position">Vedi dettaglio Dottore</router-link>
                         </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="row mt-3">
+                <!-- <h2 class="py-3 text-success">Lista dottori</h2> -->
+                <div v-if="this.store.doctors.length > 0" class="d-flex flex-wrap ms-gap">
+
+                    <div class="ms-col d-flex align-items-stretch " v-for=" doctor  in  this.store.doctors ">
+                        <div class="card my_height ">
+                            <img v-if="doctor.photo" :src="`${store.baseUrlnoApi}/storage/${doctor.photo}`"
+                                class="card-img-top my_image" :alt="`immagine-profilo-di-${doctor.user.name}`">
+                            <img v-else class="card-img-top my_image"
+                                src="https://www.diamedica.it/wp-content/uploads/2018/12/dottore-1024x1024.jpg"
+                                :alt="`immagine-profilo-di-${doctor.user.name}`">
+                            <div class="card-body position-relative">
+                                <h3 class="card-title">{{ doctor.user.name }} {{ doctor.user.surname }}</h3>
+                                <div class="badge rounded-pill text-bg-success fs-6 me-2 my-2 d-inline-block"
+                                    v-for="specialization in doctor.specializations">{{ specialization.name }}</div>
+                                <div>
+                                    <span class="card-title" v-if="doctor.review_count != 0">N. recensioni: {{
+                                        doctor.review_count
+                                    }}
+                                    </span>
+                                    <span class="card-title d-block" v-else>Nessuna recensione</span>
+                                </div>
+                                <div v-if="doctor.average_stars != 0">
+                                    <span class="card-title">Media Voti:</span>
+                                    <div class="btn-outline-dark">
+                                        <i class="fa-solid fa-star text-warning"
+                                            v-for=" littlestar  in  doctor.average_stars"></i>
+                                        <i class="fa-regular fa-star text-warning"
+                                            v-for=" littlestar  in  (5 - doctor.average_stars)"></i>
+                                    </div>
+                                </div>
+                                <span class="card-title d-block" v-else>Nessuna votazione</span>
+                                <p class="card-text mb-5">Indirizzo: {{ doctor.address }}</p>
+
+                                <router-link :to="{ name: 'doctor_page', params: { slug: doctor.slug } }"
+                                    class="btn ms-bg-primary position-absolute my_position">Vedi dettaglio
+                                    Dottore</router-link>
+                            </div>
+                        </div>
+
                     </div>
                 </div>
             </div>
         </div>
-
-        <div class="row">
-            <h2 class="py-3 text-success">Lista dottori</h2>
-            <div v-if="this.store.doctors.length > 0" class="d-flex flex-wrap ms-gap">
-
-                <div class="ms-col d-flex align-items-stretch " v-for=" doctor  in  this.store.doctors ">
-                    <div class="card my_height ">
-                        <img v-if="doctor.photo" :src="`${store.baseUrlnoApi}/storage/${doctor.photo}`"
-                            class="card-img-top my_image" :alt="`immagine-profilo-di-${doctor.user.name}`">
-                        <img v-else class="card-img-top my_image"
-                            src="https://www.diamedica.it/wp-content/uploads/2018/12/dottore-1024x1024.jpg"
-                            :alt="`immagine-profilo-di-${doctor.user.name}`">
-                        <div class="card-body position-relative">
-                            <h3 class="card-title">{{ doctor.user.name }} {{ doctor.user.surname }}</h3>
-                            <span class="card-title" v-if="doctor.review_count != 0">N. recensioni: {{ doctor.review_count
-                            }}
-                            </span>
-                            <span class="card-title d-block" v-else>Nessuna recensione</span>
-                            <div v-if="doctor.average_stars != 0">
-                                <span class="card-title">Media Voti:</span>
-                                <div class="btn-outline-dark">
-                                    <i class="fa-solid fa-star text-warning"
-                                        v-for=" littlestar  in  doctor.average_stars"></i>
-                                    <i class="fa-regular fa-star text-warning"
-                                        v-for=" littlestar  in  (5 - doctor.average_stars)"></i>
-                                </div>
-                            </div>
-                            <span class="card-title d-block" v-else>Nessuna votazione</span>
-                            <p class="card-text">Indirizzo: {{ doctor.address }}</p>
-                            <div class="mb-5">
-
-                                <span class="badge rounded-pill text-bg-success fs-6 me-2 my-2 d-inline-block"
-                                    v-for="specialization in doctor.specializations">{{ specialization.name }}</span>
-
-                            </div>
-                            <router-link :to="{ name: 'doctor_page', params: { slug: doctor.slug } }"
-                                class="btn ms-bg-primary position-absolute my_position">Vedi dettaglio Dottore</router-link>
-                        </div>
-                    </div>
-
-                </div>
-            </div>
-            <div v-else>
-                <h1>Non ci sono dottori</h1>
-            </div>
+        <div v-else class="d-flex justify-content-center p-4 fs-4">
+            <span>Nessun risultato</span>
         </div>
     </div>
 </template>
@@ -238,6 +243,10 @@ export default {
 
 .sponsoredCard {
     border: 4px solid $primary-color;
+}
+
+.ms_badge {
+    border-bottom-left-radius: 12px;
 }
 
 
